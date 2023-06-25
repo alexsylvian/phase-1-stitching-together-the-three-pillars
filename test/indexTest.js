ca9cf36cc43fc6df9629b2732dea40e5d2349f9a
alexsylvian <lazeratus1@gmail.com>
2023-06-25 18:48:51 -0400

    first roll, no change

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,6 +3,6 @@ require ( './helpers.js' );
 describe("index.js", () => {
   it("exists", () => {
     expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
+    expect(testVar).to.deep.equal({}) //with out .deep, {} does not equal {}
   })
 })
